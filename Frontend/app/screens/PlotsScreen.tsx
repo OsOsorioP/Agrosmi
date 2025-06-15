ca758cd6d88c5726355f024a257c14fc1531eb19
workspace014-admin <oscar.osorio1884@gmail.com>
2025-06-15 18:30:22 -0500

    feat: add PlotsScreen component for displaying plots

---
+++ Frontend/app/screens/PlotsScreen.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const PlotsScreen = () => {
+    return (
+        <SafeAreaView>
+
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default PlotsScreen;
