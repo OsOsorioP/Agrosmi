d4f1fa17f28749e9f1a30df5d6c5615cff16c4d0
workspace014-admin <oscar.osorio1884@gmail.com>
2025-06-15 18:29:56 -0500

    feat: create PlotCard component for displaying plot information

---
+++ Frontend/app/components/specific/PlotCard.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, TouchableOpacity } from 'react-native';
+
+const PlotCard = () => {
+    return (
+        <TouchableOpacity>
+        </TouchableOpacity>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default PlotCard;
