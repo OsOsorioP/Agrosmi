696d50c1bac0da58b5e86ecd45ad41e8b1a09714
workspace014-admin <oscar.osorio1884@gmail.com>
2025-06-15 18:30:19 -0500

    feat: create ChatScreen component for chat functionality

---
+++ Frontend/app/screens/ChatScreen.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, SafeAreaView} from 'react-native';
+const ChatScreen = () => {
+
+    return (
+        <SafeAreaView>
+        </SafeAreaView>
+    );
+};
+
+
+const styles = StyleSheet.create({
+    
+});
+
+export default ChatScreen;
