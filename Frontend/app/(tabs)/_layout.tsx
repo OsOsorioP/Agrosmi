643ccb83bd119b274a23957e0460eb3a4c2972d2
workspace014-admin <oscar.osorio1884@gmail.com>
2025-06-17 13:57:33 -0500

    feat: add TabLayout component for tab navigation structure

---
+++ Frontend/app/(tabs)/_layout.tsx
@@ -0,0 +1,51 @@
+import { Tabs } from 'expo-router';
+import { IconSymbol, MAPPING } from '@/components/ui/IconSymbol';
+
+interface props {
+  name: string;
+  title: string;
+  icon: keyof typeof MAPPING;
+}
+
+export default function TabLayout() {
+
+  const tabsScreens: props[] = [
+    { name: "index", title: "Chat", icon: "message.fill" },
+    { name: "clima", title: "Clima", icon: "cloud.fill" },
+    { name: "parcelas", title: "Parcelas", icon: "map.fill" },
+    { name: "chats", title: "Chats", icon: "archive.fill" },
+    { name: "usuario", title: "Usuario", icon: "user.fill" },
+  ]
+
+  return (
+    <Tabs
+      screenOptions={{ 
+        headerShown:false,
+        tabBarActiveTintColor: 'green', 
+        tabBarInactiveTintColor:"black",
+        tabBarBadgeStyle: {
+          backgroundColor:"#333",
+          color:"#fff"
+        },
+        animation: "fade",
+        tabBarStyle: {
+          padding:0,
+          margin:0,
+          borderRadius:16,
+        }
+      }}
+    >
+      {tabsScreens.map((screen, index) => (
+        <Tabs.Screen
+          key={index}
+          name={screen.name}
+          options={{
+            title: `${screen.title}`,
+            tabBarIcon: ({ color, size }) => <IconSymbol size={size} name={`${screen.icon}`} color={color} />,
+          }}
+        />
+      ))}
+      <Tabs.Screen name='+not-found' options={{ href: null }} />
+    </Tabs>
+  );
+}
