5aad92d000f1cf0c0cff73e46d7c321b7f9edecd
workspace014-admin <oscar.osorio1884@gmail.com>
2025-06-19 21:56:19 -0500

    feat: wrap RootLayout in SafeAreaProvider and SafeAreaView for improved layout handling

--- Frontend/app/_layout.tsx
+++ Frontend/app/_layout.tsx
@@ -2,22 +2,29 @@ import Header from '@/components/layout/Header';
 import { Text } from '@react-navigation/elements';
 import { Stack } from 'expo-router';
 import { StyleSheet, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{
-      headerShown: false,
-    }}>
-      <View>
-        <Text>fgdfgdfg</Text>
-      </View>
-      {/*<Stack.Screen name="login" options={{ headerShown: false }} />*/}
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <Stack screenOptions={{
+          headerShown: false,
+        }}>
+          <View style={{flex:1,height:90, backgroundColor:"#000"}}>
+            <Header title='fdsfs'/>
+          </View>
+          {/*<Stack.Screen name="login" options={{ headerShown: false }} />*/}
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  container:{
+    flex:1
+  }
 })
