9e75f2c0ec5129b1f482ac2e1658ae63d9ade075
workspace014-admin <oscar.osorio1884@gmail.com>
2025-06-27 17:13:42 -0500

    refactor: remove unused Header component and related imports from RootLayout

--- Frontend/app/_layout.tsx
+++ Frontend/app/_layout.tsx
@@ -1,7 +1,5 @@
-import Header from '@/components/layout/Header';
-import { Text } from '@react-navigation/elements';
 import { Stack } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
@@ -11,9 +9,6 @@ export default function RootLayout() {
         <Stack screenOptions={{
           headerShown: false,
         }}>
-          <View style={{flex:1,height:90, backgroundColor:"#000"}}>
-            <Header title='fdsfs'/>
-          </View>
           {/*<Stack.Screen name="login" options={{ headerShown: false }} />*/}
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
